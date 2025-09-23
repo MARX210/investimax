0ba9a7ce8793dbe85fa0873dd982fba678cd1003
MARX VINICIUS GAMA VIEIRA BISPO <marx@alunos.utfpr.edu.br>
2025-09-23 01:15:20 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -14,17 +14,3 @@ const pool = new Pool({
 export const db = {
   query: (text: string, params?: any[]) => pool.query(text, params),
 };
-
-// Test the connection
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack);
-  }
-  client.query('SELECT NOW()', (err, result) => {
-    release();
-    if (err) {
-      return console.error('Error executing query', err.stack);
-    }
-    console.log('Successfully connected to PostgreSQL database!');
-  });
-});
